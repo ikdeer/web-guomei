6dde8217034eeed8714bbc41cafbff48d048dbc7
孙彬超 <sunbinchao@zto.com>
2019-08-23 09:46:15 +0800

    增加首页头部组件

--- src/views/CompanyHome/home.vue
+++ src/views/CompanyHome/home.vue
@@ -2,49 +2,8 @@
     <div class="CompanyHome">
       <!-- 头部区域 header -->
       <header class="home-header">
-        <nav class="header-nav">
-          <div class="header-nav_Left">
-            <div class="header-navLeft">
-              <img class="navLeft-Img" src="/static/images/logo_image@2x.png" alt="">
-              <div class="navLeft-Text">
-                <span></span>
-                <p>人脸认证开放平台</p>
-              </div>
-            </div>
-            <div class="header-navCenter">
-              <ul class="navCenter-ul">
-                <li>
-                  <span>产品服务</span>
-                  <ul class="navCenter-ulMenu">
-                    <li>
-                      <span>技术文档</span>
-                    </li>
-                    <li>
-                      <span>产品服务</span>
-                    </li>
-                    <li>
-                      <span>接入须知</span>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>解决方案</span>
-                </li>
-                <li>
-                  <span>技术文档</span>
-                </li>
-                <li>
-                  <span>接入须知</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="header-navRight">
-            <el-button type="text" class="button-login">登陆</el-button>
-            <el-button type="text" class="button-register">注册</el-button>
-            <el-button class="button-console">控制台</el-button>
-          </div>
-        </nav>
+        <!-- 头部公用组件 -->
+        <Header_Nav></Header_Nav>
         <div class="header-IfoImg">
           <div class="IfoImg-left">
             <h4 class="IfoImg-Title">人脸对比</h4>
@@ -135,8 +94,10 @@
 </template>
 
 <script>
+    import Header_Nav from '@/views/CompanyHome/component/header/HeaderNav'
     export default {
         name: "home",
+        components:{Header_Nav},
         data(){
           return {
 
@@ -150,134 +111,6 @@
   width: 100%;
   .home-header{
     width: 100%;
-    .header-nav{
-      width: 100%;
-      height: 0.8rem;
-      background: #1B1B1B;
-      display: flex;
-      display: -webkit-flex;
-      align-items: center;
-      justify-content: space-between;
-      .header-nav_Left{
-        width: 75%;
-        display: flex;
-        display: -webkit-flex;
-        align-items: center;
-        .header-navLeft{
-          display: flex;
-          display: -webkit-flex;
-          align-items: center;
-          .navLeft-Img{
-            width: 1.38rem;
-            height: 0.46rem;
-            display: block;
-            margin-left: 0.4rem;
-          }
-          .navLeft-Text{
-            display: flex;
-            display: -webkit-flex;
-            align-items: center;
-            >span{
-              width: 0.02rem;
-              height: 0.3rem;
-              display: block;
-              background: #ffffff;
-              margin:0 0.2rem;
-            }
-            >p{
-              font-size: 0.26rem;
-              color: #ffffff;
-              font-weight: 600;
-            }
-          }
-        }
-        .header-navCenter{
-          margin-left: 0.82rem;
-          .navCenter-ul{
-            display: flex;
-            display: -webkit-flex;
-            align-items: center;
-            >li{
-              width: 1.6rem;
-              height:0.8rem;
-              display: flex;
-              display: -webkit-flex;
-              align-items: center;
-              justify-content: center;
-              position: relative;
-              span{
-                font-size: 0.2rem;
-                color: #ffffff;
-              }
-              .navCenter-ulMenu{
-                width: 1.6rem;
-                padding: 0.2rem 0;
-                position: absolute;
-                left: 0;
-                top: 0.8rem;
-                background: #1A1A1A;
-                display: none;
-                >li{
-                  display: flex;
-                  display: -webkit-flex;
-                  align-items: center;
-                  justify-content: center;
-                  >span{
-                    font-size: 0.18rem;
-                    padding: 0.09rem 0.18rem;
-                    color: #ffffff;
-                  }
-                }
-                >li:hover{
-                  >span{
-                    background: #2B2B2B;
-                    color: #F20A59;
-                  }
-                }
-              }
-            }
-            >li:hover{
-              background: #000000;
-              .navCenter-ulMenu{
-                display: block;
-              }
-              >span{
-                color: #F20A59;
-                line-height: 0.8rem;
-                border-bottom: 0.04rem solid #F20A59;
-              }
-            }
-          }
-        }
-      }
-
-      .header-navRight{
-        width: 25%;
-        display: flex;
-        display: -webkit-flex;
-        align-items: center;
-        justify-content: flex-end;
-        margin-right: 0.4rem;
-        .button-login{
-          font-size: 0.2rem;
-          color: #ffffff;
-        }
-        .button-register{
-          font-size: 0.2rem;
-          color: #ffffff;
-          margin-left: 0.36rem;
-          margin-right: 0.3rem;
-        }
-        .button-console{
-          padding: 0.11rem 0.45rem;
-          border-radius: 0.18rem;
-          font-size: 0.2rem;
-          color: #ffffff;
-          background:#F20A59;
-          border: none;
-        }
-      }
-    }
     .header-IfoImg{
       width: auto;
       height: 5.8rem;
@@ -288,6 +121,7 @@
       justify-content: space-between;
       padding-left: 1.5rem;
       padding-right: 2.12rem;
+      margin-top: 0.8rem;
       .IfoImg-left{
         .IfoImg-Title{
           font-size: 0.68rem;
