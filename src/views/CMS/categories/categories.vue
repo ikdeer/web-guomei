db4e08c49521a164b03baa4aaace1022d4967ed1
孙彬超 <sunbinchao@zto.com>
2019-09-07 15:04:06 +0800

    修改技术文档编辑器一些配置

--- src/views/CMS/categories/categories.vue
+++ src/views/CMS/categories/categories.vue
@@ -12,8 +12,8 @@
           <el-tab-pane label="技术文档栏目配置">
             <categoriesModule></categoriesModule>
           </el-tab-pane>
-          <el-tab-pane label="首页tab栏目配置">消息中心</el-tab-pane>
           <el-tab-pane label="接入须知栏目配置">角色管理</el-tab-pane>
+          <el-tab-pane label="首页tab栏目配置">消息中心</el-tab-pane>
         </el-tabs>
       </div>
     </div>
