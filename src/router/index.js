c5b56ec3c04a30faa71e8af13056648138181c1f
孙彬超 <sunbinchao@zto.com>
2019-09-19 20:25:55 +0800

    修改bug

--- src/router/index.js
+++ src/router/index.js
@@ -52,7 +52,7 @@ Router.prototype.push = function push(location) {
 Vue.use(Router);
 
 const router = new Router({
-  mode:'history',
+  mode:'hash',
   //路由跳转后页面回到顶部
   scrollBehavior: () => ({ y: 0 }),
   routes: [
