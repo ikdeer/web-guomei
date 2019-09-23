00224cf0da4331c83e41aae77a26ff51822302f9
孙彬超 <sunbinchao@zto.com>
2019-09-23 15:10:40 +0800

    修改bug

--- src/views/CMS/banner/bannerAdd.vue
+++ src/views/CMS/banner/bannerAdd.vue
@@ -36,8 +36,8 @@
                 </el-upload>
               </div>
             </el-form-item>
-            <el-form-item label="活动区域">
-              <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-form-item label="活动区域" prop="differentiate">
+              <el-select v-model="form.differentiate" placeholder="请选择活动区域">
                 <el-option label="区域一" value="shanghai"></el-option>
                 <el-option label="区域二" value="beijing"></el-option>
               </el-select>
