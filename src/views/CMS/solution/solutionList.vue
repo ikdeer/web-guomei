191413f9c36f6a26a72074bcabd7ee8b9e905e27
chengzhongkai <chengzhongkai@zto.cn>
2019-09-23 14:48:25 +0800

    解决方案修改

--- src/views/CMS/solution/solutionList.vue
+++ src/views/CMS/solution/solutionList.vue
@@ -134,7 +134,8 @@ export default {
     },
     //批量删除
     ClickBatchDeLeTe(){
-      if(this.DeleteArr.length != 0){
+        console.log(this.DeleteArr);
+        if(this.DeleteArr.length != 0){
         this.$confirm('此操作将永久删除数据, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
